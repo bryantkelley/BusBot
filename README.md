3e1f300f74d13ecd2ede39c96010c19eaf71b813
Bryant Kelley <bryant@bryantkelley.com>
2025-10-02 14:29:30 -0700

    Add script to convert GTFS data files to json, document some setup steps

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 BusBot provides information about King County Metro buses and routes over MeshCore. Transit scheduling, geographic, and real-time data provided by permission of King County.
 
 ## Getting Started
+Download and extract the GTFS base data available from [King County Metro](https://kingcounty.gov/en/dept/metro/rider-tools/mobile-and-web-apps#toc-developer-resources) to the metro/txt folder.
+Add the GTFS-RT Trip Updates and Service Alerts feeds to the .env file.
---
+++ scripts/convert_txt.js
@@ -0,0 +1,51 @@
+import fs from "node:fs";
+import path from "node:path";
+
+const inputFolder = "./metro/txt";
+const outputFolder = "./metro/json";
+
+try {
+  if (!fs.existsSync(inputFolder)) {
+    throw new Error("Input files not found!");
+  }
+
+  if (!fs.existsSync(outputFolder)) {
+    fs.mkdirSync(outputFolder);
+  }
+
+  const isFile = (fileName) => {
+    return fs.lstatSync(fileName).isFile();
+  };
+
+  const files = fs
+    .readdirSync(inputFolder)
+    .map((fileName) => path.join(inputFolder, fileName))
+    .filter(isFile);
+
+  files.forEach((fileName) => {
+    try {
+      const data = fs
+        .readFileSync(fileName, "utf8")
+        .split(/\r?\n|\r|\n/g)
+        .map((line) => line.split(","));
+      const keys = data[0];
+      const outputData = [];
+
+      for (let dataIndex = 1; dataIndex < data.length; dataIndex++) {
+        if (data[dataIndex].length === keys.length) {
+          let row = {};
+          for (let keyIndex = 0; keyIndex < keys.length; keyIndex++) {
+            row[keys[keyIndex]] = data[dataIndex][keyIndex];
+          }
+          outputData.push(row);
+        }
+      }
+
+      fs.writeFileSync(fileName.replaceAll("txt", "json"), JSON.stringify(outputData, null, 2));
+    } catch (err) {
+      console.error(err);
+    }
+  });
+} catch (e) {
+  console.error(e);
+}
